8b9a9d235f70f7a4f4b590aff8b31c63e71b62d1
krouspy <knjlau1@gmail.com>
2021-01-20 11:15:00 +0100

    add swagger

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes');
+const swagger = require('./swagger');
 const { notFound, errorHandler } = require('./middlewares');
 // eslint-disable-next-line camelcase
 const { __cors_origin__ } = require('./config');
@@ -15,6 +16,8 @@ app.use(cors({ origin: __cors_origin__ }));
 app.use(express.json());
 
 app.use('/api', routes);
+
+swagger(app);
 
 app.use(notFound);
 app.use(errorHandler);
