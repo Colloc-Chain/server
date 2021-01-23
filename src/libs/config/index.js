7f25a1d511237ce8e2dfb57fcb7ae8640be6ee69
krouspy <knjlau1@gmail.com>
2021-01-23 14:44:44 +0100

    fund accounts on creation

--- src/libs/config/index.js
+++ src/libs/config/index.js
@@ -6,10 +6,14 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const __web3_uri__ = isProduction ? process.env.WEB3_URI_PROD : process.env.WEB3_URI_DEV;
 const __mongo_uri__ = isProduction ? process.env.MONGO_URI : process.env.MONGO_URI_DEV;
+const __node_private_key__ = process.env.NODE_PRIVATE_KEY;
+const __eth_to_fund__ = process.env.ETH_TO_FUND;
 const __jwt_secret__ = process.env.JWT_SECRET;
 
 module.exports = {
   __web3_uri__,
   __mongo_uri__,
+  __node_private_key__,
+  __eth_to_fund__,
   __jwt_secret__,
 };
